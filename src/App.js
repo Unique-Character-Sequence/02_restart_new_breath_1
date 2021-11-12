62f2c306311c0d6639d44571464f156213a58195
Unique-Character-Sequence <comingdoomsday@gmail.com>
2021-11-12 21:06:35 +0300

    test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,34 @@ import React from "react";
 const App = () => {
     return (
         <div className="App">
-            sss
+            <Header/>
+            А шо за текст тут у нас, гиги-гага
+            <ListOfThoughts/>
             <img className="App-logo" src={logo} alt=""/>
+        </div>
+    )
+}
+
+let ListOfThoughts = () => {
+    return <div> {/* Видал прикол, можно и в скобочки не брать в такой ситуации*/}
+            <ul>
+                <li>Итак, братан</li>
+                <li>Поздравляю тебя с новым началом</li>
+                <li>Ты быстренько восстановишься, да!</li>
+                <li>А потом и прогресс продолжишь</li>
+                <li>После этого и деньжат заработаешь</li>
+                <li>Боже! ;') я даже завидую тебе...</li>
+            </ul>
+        </div>
+
+}
+
+let Header = () => {
+    return (
+        <div>
+            <a href="#s">rofl1</a>
+            <a href="#s">rofl2</a>
+            <a href="#s">rofl3</a>
         </div>
     )
 }
