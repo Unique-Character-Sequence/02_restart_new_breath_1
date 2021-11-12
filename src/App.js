6618f8ed5f3e25228f3713eba909f76e50e31a5f
Unique-Character-Sequence <comingdoomsday@gmail.com>
2021-11-12 20:33:17 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">S
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload qqqq.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            sss
+            <img className="App-logo" src={logo} alt=""/>
+        </div>
+    )
 }
 
 export default App;
