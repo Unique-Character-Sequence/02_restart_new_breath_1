0a1e41a96025d494dea44c72b2c0c5a08c9990f8
Unique-Character-Sequence <comingdoomsday@gmail.com>
2021-11-17 19:39:29 +0300

    Check commit

---
+++ src/Components/Profile/ProfilePostFeed/ProfilePostFeed.jsx
@@ -0,0 +1,12 @@
+import ProfilePost from "./ProfilePost/ProfilePost";
+
+import cutepfp from '../../../cutepfp.jpg'
+import heart from '../../../heart.jpg'
+
+const ProfilePostFeed = () => {
+    return <div>
+        <ProfilePost post_id='1'/>
+    </div>
+}
+
+export default ProfilePostFeed;
