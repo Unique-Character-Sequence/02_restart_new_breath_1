bc4f90a122b986cccb0652aa7c92a2a3d8d46ad0
Unique-Character-Sequence <comingdoomsday@gmail.com>
2021-11-19 14:41:43 +0300

    [+] state разделили на категории/ветки

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,21 +1,25 @@
 let state = {
-    rawMsgContentDatasets: [
-        {text: 'Сообщенивание 1'},
-        {text: 'Сообщенивание 2'},
-        {text: 'Сообщенивание 3'},
-        {text: 'Сообщенивание 4'},
-    ],
-    rawPostDatasets: [
-        {post_id: '1', author: 'Абобус Алексеич', likes_amount: '99', text: 'Флексанул конкретно'},
-        {post_id: '2', author: 'Бибибус Чечевичкин', likes_amount: '299', text: 'Очень конкретно'},
-        {post_id: '3', author: 'Бибибус Кукурача', likes_amount: '19', text: 'Жёстким флексом'},
-    ],
-    rawUserTileDatasets: [
-        {id: 1, name: 'Бибус Челикович'},
-        {id: 2, name: 'Абобус Беброчевских'},
-        {id: 3, name: 'Бибабоба Кринжов'},
-        {id: 4, name: 'Бублик Бибубликовый'},
-    ]
+    MessagesPageDatasets: {
+        rawUserTileDatasets: [
+            {id: 1, name: 'Бибус Челикович'},
+            {id: 2, name: 'Абобус Беброчевских'},
+            {id: 3, name: 'Бибабоба Кринжов'},
+            {id: 4, name: 'Бублик Бибубликовый'},
+        ],
+        rawMsgContentDatasets: [
+            {text: 'Привет, буброчелик!', myMsg: true},
+            {text: 'Привеееееет, шо ты, как ты, чё дышишь', myMsg: false},
+            {text: 'Да вот, взгрустнул чё-т немного, но, знай, мы не одни', myMsg: true},
+            {text: 'Сообщенивание 4', myMsg: false},
+        ],
+    },
+    ProfilePageDatasets: {
+        rawPostDatasets: [
+            {post_id: '1', author: 'Абобус Алексеич', likes_amount: '99', text: 'Флексанул конкретно'},
+            {post_id: '2', author: 'Бибибус Чечевичкин', likes_amount: '299', text: 'Очень конкретно'},
+            {post_id: '3', author: 'Бибибус Кукурача', likes_amount: '19', text: 'Жёстким флексом'},
+        ],
+    }
 }
 
 export default state
