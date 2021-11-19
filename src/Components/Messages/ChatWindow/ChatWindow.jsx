05b50a120df32a1992140c36bc1fc44b872dd33f
Unique-Character-Sequence <comingdoomsday@gmail.com>
2021-11-19 14:41:43 +0300

    [+] state разделили на категории/ветки

--- src/Components/Messages/ChatWindow/ChatWindow.jsx
+++ src/Components/Messages/ChatWindow/ChatWindow.jsx
@@ -2,7 +2,6 @@ import MessageTile from "./MessageTile/MessageTile";
 
 const ChatWindow = (props) => {
     let doneMsgContentDatasets = props.rawMsgContentDatasets.map(obj => <MessageTile {...obj}/>)
-
     return <div>
         {doneMsgContentDatasets}
     </div>
