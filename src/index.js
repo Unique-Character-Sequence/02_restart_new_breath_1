0f6de5693a5c25d3969d1455d00c35a7849b750b
Unique-Character-Sequence <76056880+Unique-Character-Sequence@users.noreply.github.com>
2021-11-25 14:50:18 +0300

    [+] Теперь с функцией connect нет необходимости подписываться в index.js

--- src/index.js
+++ src/index.js
@@ -6,27 +6,17 @@ import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 
-let rerender = (state) => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App
-                    state={state}
-                    store={store}
-                    dispatch={store.dispatch.bind(store)}
-                    // Теперь this для этих методов это только store
-                />
-            </Provider>
-        </BrowserRouter>,
-        document.getElementById('root')
-    );
-}
 
-rerender(store.getState());
-
-
-store.subscribe(() => {
-    // Данная переданная функция будет вызываться
-    let state = store.getState()
-    rerender(state)
-});
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App
+                state={state}
+                store={store}
+                dispatch={store.dispatch.bind(store)}
+                // Теперь this для этих методов это только store
+            />
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
