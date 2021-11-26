517e7d8c3e8712a6fc94e5d221f9aafbb9a685fb
Unique-Character-Sequence <76056880+Unique-Character-Sequence@users.noreply.github.com>
2021-11-26 16:53:19 +0300

    [Fix] Friends без side-effect'а

--- src/Components/Friends/Friends.jsx
+++ src/Components/Friends/Friends.jsx
@@ -3,19 +3,22 @@ import UserTile from "./UserTile/UserTile";
 import axios from "axios"
 
 const Friends = (props) => {
-    if (props.rawUsersDatasets.length === 0) {
-        // после
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(
-                response => {
-                    console.log(response.data)
-                    props.setUsersDatasets(response.data.items)
-                })
+    let getUsers = () => {
+        if (props.rawUsersDatasets.length === 0) {
+            // после
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(
+                    response => {
+                        console.log(response.data)
+                        props.setUsersDatasets(response.data.items)
+                    })
+        }
     }
 
     let doneUsersDatasets = props.rawUsersDatasets.map(obj => <UserTile
         switchFollowedStatus={props.switchFollowedStatus} {...obj} />)
     return <div className={c.main}>
+        <button onClick={getUsers}>Get Users</button>
         {doneUsersDatasets}
     </div>
 }
