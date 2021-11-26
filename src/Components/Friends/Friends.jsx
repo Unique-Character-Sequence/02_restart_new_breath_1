9bda55ad7dde2b99d5f238cecb59b90cdf141f3d
Unique-Character-Sequence <76056880+Unique-Character-Sequence@users.noreply.github.com>
2021-11-26 19:32:15 +0300

    [+] Friends использует хук useEffect componentDidMount

--- src/Components/Friends/Friends.jsx
+++ src/Components/Friends/Friends.jsx
@@ -1,20 +1,23 @@
 import c from './Friends.module.css'
 import UserTile from "./UserTile/UserTile";
 import axios from "axios"
+import {useEffect} from 'react'
 
 const Friends = (props) => {
     let getUsers = () => {
-        if (props.rawUsersDatasets.length === 0) {
-            // после
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(
-                    response => {
-                        console.log(response.data)
-                        props.setUsersDatasets(response.data.items)
-                    })
-        }
+        // TODO: Оп
+        // FIXME: Можно и так записать
     }
-
+    useEffect(() => {
+        // componentDidMount
+        console.log('componentDidMount')
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(
+                response => {
+                    console.log(response.data)
+                    props.setUsersDatasets(response.data.items)
+                })
+    }, [])
     let doneUsersDatasets = props.rawUsersDatasets.map(obj => <UserTile
         switchFollowedStatus={props.switchFollowedStatus} {...obj} />)
     return <div className={c.main}>
